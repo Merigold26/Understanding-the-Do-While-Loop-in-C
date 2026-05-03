4b3bf9bbbd5bf312aa2b2cf9c3f6808aa2cc1741
Alper Cantekin <128486707+Merigold26@users.noreply.github.com>
2026-05-03 23:44:29 +0300

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//do-while için koþul saðlanmasa bile bir kere iþlem gerçekleþir önce iþlem gerçekleþir koþul saðlanýyor mu diye saðlanmaz ise bir kez yazar 
+main()
+{
+int i;
+i=1;//11 yazsaydýk bir kez yazýp býrakýrdý
+do
+{
+	printf("dafo \n");
+	i++;
+	}	
+while (i<=10);
+
+}
+
